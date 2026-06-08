138d30b48926bfebd5bf81e375cc04c77462fa38
Jose Angel Huerta Rios <ppangelhr@gmail.com>
2026-06-08 14:23:38 -0600

    cambios de waypoints

--- puzzlebot_final/puzzlebot_final/waypoint_manager.py
+++ puzzlebot_final/puzzlebot_final/waypoint_manager.py
@@ -18,11 +18,12 @@ class WaypointManager(Node):
         # RUTA DEL LABERINTO (Un solo destino final)
         # Formato: (X, Y)
         self.waypoints = [
-            (2.85, -0.30), #P1
-            # (0.3, -1.6), #P2
-            # (1.5, -2.75), #P3
-            # (2.85, -2.75), #P4
-            # (1.2, -1.5), #P5
+            # (36, -27.5), #P1
+            # (.36, -.94), #P2
+            # (1.70, -2.04), #P3
+            # (.36, -2.72), #P4
+            # (2.18, -.30), #P5
+            (2.72, -2.72), #P6
         ]
         
         self.current_index = 0
